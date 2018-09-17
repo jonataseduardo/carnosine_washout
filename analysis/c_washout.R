c87b465ab886152d5c07aa1290bd0c96b9483423
Jonatas Cesar <jonataseduardo@gmail.com>
2018-09-17 17:40:12 -0500

    check time pval

--- analysis/c_washout.R
+++ analysis/c_washout.R
@@ -4,22 +4,19 @@ library(brms)
 
 washout_raw <- fread('carnosine_data.csv')
 
-washout <- melt(washout_raw, id = c('group_id', 'sample_id'), 
-                variable.name = 'week_id')
+washout_0 <- 
+  melt(washout_raw, id = c('group_id', 'sample_id'), 
+       variable.name = 'week_id')
 
 week_info <- 
   data.table(week_id= c("PRE_S01", "POS_S02", "W01", 
                         "W02", "W03", "W04", "W05", "W06"),
              time_stamp = c(NA, 0, 1, 2, 4, 8, 12, 16)) 
 
-washout <- washout[week_info, on = c("week_id")]
-
+washout <- washout_0[week_info, on = c("week_id")][!is.na(value)]
 
 # First we will test if the samples in the target group are draw from the
 # sample distribution from the control at time 0
-
-ks.test(washout[is.na(time_stamp) & group_id == 'T', value], 
-        washout[is.na(time_stamp) & group_id == 'C', value])
 
 ## Result
 #D = 0.36364, p-value = 0.7473
@@ -28,34 +25,58 @@ ks.test(washout[is.na(time_stamp) & group_id == 'T', value],
 # other
 
 # Withing individual variaility is lower then the group variability analisys 
-washout[ group_id == 'C', var(value, na.rm = TRUE)]
-washout[ group_id == 'C', var(value, na.rm = TRUE), by = sample_id]
-
-#w_off <- 
-#  washout_raw[,lapply(names(w_treated)[3:9], 
-#                       function(x) get(x) - get('PRE_S01'))] 
-#
-#names(w_off) <- names(w_treated)[3:9]
-#w_off[, sample_id := w_treated[, sample_id]]
-
-decay_sample <- 
-  washout[!is.na(value) & is.na(time_stamp), 
-          .(sample_id, group_id, value, time_stamp)]
-
-decay_sample[, sample_id := as.factor(sample_id)]
-decay_sample[, group_id := as.factor(group_id)]
+washout[group_id == 'C', var(value)]
+washout[group_id == 'C', var(value), by = sample_id]
 
 
+t.test(washout[is.na(time_stamp) & group_id == 'T', value], 
+       washout[is.na(time_stamp) & group_id == 'C', value])
 
+wilcox.test(washout[is.na(time_stamp) & group_id == 'T', value], 
+            washout[is.na(time_stamp) & group_id == 'C', value])
 
-linear_fit <- 
+# Sencond we want to test if the distribution of the cardosine in the subjects
+# are diferent for each time 
+
+init_s <- washout[is.na(time_stamp) & group_id == 'T', value]
+
+diff_time_tests <- 
+  washout[!is.na(time_stamp) & time_stamp != 1 & group_id == 'T', 
+          .(wc_pval = wilcox.test(init_s, value, alternative = 'less')$p.value,
+            t_pval = t.test(init_s, value, alternative = 'less')$p.value), 
+          by = time_stamp]
+
+diff_time_tests
+
+#Classical and Bayesian statist analysis for linear model 
+classical_linear <- 
+  lm(formula = value ~ time_stamp, 
+     data = washout[group_id == 'T' & !is.na(time_stamp)])
+
+summary(classical_linear)
+
+bayes_linear <- 
   brm(value ~ time_stamp,
-      data = decay_sample[group_id == 'T'])
+      data = washout[group_id == 'T' & !is.na(time_stamp)])
 
-summary(linear_fit)
-plot(linear_fit)
-plot(marginal_effects(linear_fit), points = TRUE)
+summary(bayes_linear)
+plot(bayes_linear)
+plot(marginal_effects(bayes_linear), points = TRUE)
 
+
+classical_exp <- 
+  nls( value ~ SSasymp(time_stamp, Asym, R0, lrc), 
+      data = washout[group_id == 'T' & !is.na(time_stamp)])
+
+summary(classical_exp)
+
+ggplot(data = washout[group_id == 'T' & !is.na(time_stamp)], 
+       aes(x = time_stamp, y = value)) +
+  geom_point() + 
+  stat_smooth(method = 'nls',
+              formula = y ~ SSasymp(x, Asym, R0, lrc),
+              se = FALSE
+              )
 
 mu_control <- 
   washout[ group_id == 'C' | week_id == 'PRE_S01', 
@@ -73,56 +94,100 @@ sigma_0 <-
           sd(value, na.rm = TRUE)] 
 
 
-washout[,(max(value, na.rm = TRUE) - min(value, na.rm = TRUE))/16 ] 
-(mu_0 - mu_control) / 16
-
-3.276344 - 1.323023
-
-
-sigma_0 + sigma_control
-
 exp_prior <- 
   prior(normal(21.16837, 17.43511), nlpar = "b1") +
-  #prior(normal(1.323023, 1.953321), lb = 0, nlpar = "b2" ) +
-  prior(gamma(2,1), lb = 0, nlpar = "b2" ) +
+  prior(normal(0.5, 1), lb = 0, nlpar = "b2" ) +
   prior(normal(19.58739, 5.145628), nlpar = "b3") 
 
-exponetial_fit <- 
+bayes_exp <- 
   brm(bf(value  ~ b1 * exp(- b2 * time_stamp) + b3,  
          b1 ~ 1,
          b2 ~ 1,
          b3 ~ 1, 
          nl = TRUE,
          cmc = FALSE),
-      data = decay_sample[group_id == 'T'& !is.na(time_stamp)], 
+      data = washout[group_id == 'T'& !is.na(time_stamp)], 
       prior = exp_prior,
       control = list(adapt_delta = 0.99))
 
-summary(linear_fit)
-summary(exponetial_fit)
-plot(exponetial_fit)
-18.53 * 0.47
+summary(bayes_exp)
+plot(bayes_exp)
+plot(marginal_effects(bayes_exp), points = TRUE)
 
-gpl <- plot(marginal_effects(exponetial_fit), points = TRUE)
+loo(bayes_linear, bayes_exp)
 
-loo(exponetial_fit, linear_fit)
+##########################################
+#Same analysis removing offset 
+###########################################
 
-fit_classical <- 
+w_off <- 
+  washout_raw[,lapply(names(washout_raw)[3:9], 
+                       function(x) get(x) - get('PRE_S01'))] 
+
+names(w_off) <- names(washout_raw)[3:9]
+w_off[, sample_id := washout_raw[, sample_id]]
+w_off[, group_id := washout_raw[, group_id]]
+
+lw_off <- 
+  melt(w_off, id = c('group_id', 'sample_id'), 
+       variable.name = 'week_id')
+
+washout_off <- lw_off[week_info, on = c("week_id")][!is.na(value)]
+
+
+#Classical and Bayesian statist analysis for linear model 
+classical_linear_off <- 
+  lm(formula = value ~ time_stamp, 
+     data = washout_off[group_id == 'T'])
+
+summary(classical_linear_off)
+summary(classical_linear)
+
+bayes_linear_off <- 
+  brm(value ~ time_stamp,
+      data = washout_off[group_id == 'T'])
+
+prior_summary(bayes_linear)
+
+summary(bayes_linear_off)
+plot(bayes_linear_off)
+plot(marginal_effects(bayes_linear_off), points = TRUE)
+
+classical_exp_off <- 
   nls( value ~ SSasymp(time_stamp, Asym, R0, lrc), 
-      data = decay_sample[group_id == 'T' & !is.na(time_stamp)])
+      data = washout_off[group_id == 'T'])
 
-summary(fit_classical)
+summary(classical_exp_off)
 
-gg <- decay_sample[group_id == 'T' & !is.na(time_stamp)]
-ggplot(gg, aes(x = time_stamp, y = value)) +
+ggplot(data = washout_off[group_id == 'T'], 
+       aes(x = time_stamp, y = value)) +
   geom_point() + 
   stat_smooth(method = 'nls',
               formula = y ~ SSasymp(x, Asym, R0, lrc),
               se = FALSE
               )
 
+exp_prior_off <- 
+  prior(normal(21.16837, 17.43511), nlpar = "b1") +
+  prior(normal(0.5, 1), lb = 0, nlpar = "b2" )
+
+bayes_exp_off <- 
+  brm(bf(value  ~ b1 * exp(- b2 * time_stamp),  
+         b1 ~ 1,
+         b2 ~ 1,
+         nl = TRUE,
+         cmc = FALSE),
+      data = washout_off[group_id == 'T'], 
+      prior = exp_prior_off,
+      control = list(adapt_delta = 0.99))
+
+summary(bayes_exp_off)
+plot(bayes_exp_off)
+plot(marginal_effects(bayes_exp_off), points = TRUE)
+
+loo(bayes_linear_off, bayes_exp_off)
+
 ggplot(washout, aes(x = time_stamp, y = value, color = sample_id)) +
   geom_point() + 
   geom_line() + 
   facet_wrap(~ group_id)
-
